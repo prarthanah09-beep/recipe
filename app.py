8032f3694525fdfe8349553493495c0042e13683
prarthanah09-beep <prarthanah09@gmail.com>
2026-07-17 16:32:55 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ st.title("🍽️ Recipe Recommendation System")
 # Load CSV
 df = pd.read_csv("recipes.csv")
 
-# Remove extra spaces from column names
+# Remove extra spaces
 df.columns = df.columns.str.strip()
 
 # Convert ingredients to lowercase
 df["Ingredients"] = df["Ingredients"].astype(str).str.lower()
 
-# Search ingredient
 ingredient = st.text_input("Enter ingredient")
 
 if ingredient:
@@ -20,43 +19,37 @@ if ingredient:
 
     result = df[df["Ingredients"].str.contains(ingredient, case=False, na=False)]
 
-    if len(result) > 0:
+    if not result.empty:
         st.success("Recipes Found")
 
-        for i, row in result.iterrows():
+        for _, row in result.iterrows():
 
             st.subheader("🍴 " + row["Recipe"])
+            st.write("**Ingredients:**", row["Ingredients"])
+            st.write("**Cuisine:**", row["Cuisine"])
+            st.write("**Region:**", row["Region"])
 
-            st.write("Ingredients:", row["Ingredients"])
-            st.write("Cuisine:", row["Cuisine"])
-            st.write("Region:", row["Region"])
+            # Open recipe pages
+            if row["Recipe"] == "Pizza":
+                st.page_link("pages/Pizza.py", label="🍕 Open Pizza Recipe")
 
-            # Open Recipe Pages
-
-            if row["Recipe"].strip() == "Pizza":
-                st.page_link(
-                    "pages/Pizza.py",
-                    label="🍕 Open Pizza Recipe"
-                )
-
-            elif row["Recipe"].strip() == "Veg Fried Rice":
+            elif row["Recipe"] == "Veg Fried Rice":
                 st.page_link(
                     "pages/Veg_Fried_Rice.py",
                     label="🍚 Open Veg Fried Rice Recipe"
                 )
-            elif row["Recipe"].strip() == "Butter Chicken":
-             st.page_link(
-        "pages/Butter_chicken.py",
-        label="🍗 Open Butter Chicken Recipe"
-    )
+
+            elif row["Recipe"] == "Butter Chicken":
+                st.page_link(
+                    "pages/Butter_chicken.py",
+                    label="🍗 Open Butter Chicken Recipe"
+                )
 
             st.divider()
 
     else:
-        st.error(f"No matches found for {ingredient}")
+        st.error(f"No matches found for '{ingredient}'")
 
-
-# Display database
 st.subheader("📋 Recipe Database")
 st.dataframe(df)
 
